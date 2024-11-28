9fa862536210f2a5d1bc5f8b1dd118d087b72bda
Akeem Hart <akeemlhart@gmail.com>
2024-11-28 15:49:06 +0000

    feat: Adding installation video

---
+++ setup-videos/org-locations.js
@@ -0,0 +1,27 @@
+var addressPoints = [
+  [
+    "Queen Mary University of London, Mile End, London, United Kingdom",
+    51.52492685,
+    -0.03924405317429028
+  ],
+  [
+    "King's College London, London, United Kingdom",
+    51.5060001,
+    -0.1121582948025203
+  ],
+  [
+    "University of Chicago, Chicago, Illinois, United States",
+    41.79139685,
+    -87.60084387193544
+  ],
+  [
+    "Lady Margaret Hall, Oxford, United Kingdom",
+    51.76568035,
+    -1.2529776819677187
+  ],
+  [
+    "Wilson Hall, Fermilab, Illinois, United States",
+    41.83825925,
+    -88.26171241235335
+  ]
+];
